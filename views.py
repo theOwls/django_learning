1eff81cd00e80d103d4f0ad2ee2baa4442191c11
theOwls <woodliam003@gmail.com>
2017-01-29 20:17:29 +0000

    changing everything

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse("My name is Liam, I like code.")
 
 # Create your views here.
